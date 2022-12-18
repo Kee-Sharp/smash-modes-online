166dc5edceede2473d16760b41e36138a5af6377
Spencer <keetwosuccess@gmail.com>
2022-12-18 06:00:06 -0500

    Created individual rooms each with their own set of data. Setup options moved to firebase instead of stored locally

--- src/Setup.js
+++ src/Setup.js
@@ -1,15 +1,15 @@
-import React, { useState } from "react";
-import { Button, FormControl, Form, Col } from "react-bootstrap";
-import _ from "lodash";
+import React, { useState } from 'react';
+import { Button, FormControl, Form, Col } from 'react-bootstrap';
+import _ from 'lodash';
 
 export const Setup = ({ fighterLength, startGame }) => {
   const [maxBattles, setMaxBattles] = useState(1);
-  const [name1, setName1] = useState("p1");
-  const [name2, setName2] = useState("p2");
+  const [name1, setName1] = useState('p1');
+  const [name2, setName2] = useState('p2');
   const [mercy, setmercy] = useState(false);
   const absoluteMax = Math.floor(fighterLength / 2);
   return (
-    <div style={{ display: "flex", justifyContent: "center" }}>
+    <div style={{ display: 'flex', justifyContent: 'center' }}>
       <Form id="setup">
         <Form.Group>
           <Form.Label>How Many Battles?</Form.Label>
@@ -29,17 +29,11 @@ export const Setup = ({ fighterLength, startGame }) => {
         <Form.Row>
           <Form.Group as={Col}>
             <Form.Label>P1's name</Form.Label>
-            <FormControl
-              value={name1}
-              onChange={e => setName1(e.target.value)}
-            />
+            <FormControl value={name1} onChange={e => setName1(e.target.value)} />
           </Form.Group>
           <Form.Group as={Col}>
             <Form.Label>P2's name</Form.Label>
-            <FormControl
-              value={name2}
-              onChange={e => setName2(e.target.value)}
-            />
+            <FormControl value={name2} onChange={e => setName2(e.target.value)} />
           </Form.Group>
         </Form.Row>
         <Form.Group>
@@ -53,7 +47,10 @@ export const Setup = ({ fighterLength, startGame }) => {
         <Button
           variant="primary"
           type="submit"
-          onClick={() => startGame(maxBattles, name1, name2, mercy)}
+          onClick={e => {
+            e.preventDefault();
+            startGame(maxBattles, name1, name2, mercy);
+          }}
         >
           Continue
         </Button>
